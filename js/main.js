20478476bccb9edd5b7e457f22766027e6d61650
板垣恵 <mm.cosmos202@gmail.com>
2018-11-10 14:31:38 +0900

    [add] フリックの速度取得

--- js/main.js
+++ js/main.js
@@ -1,18 +1,26 @@
-var startX, startY, endX, endY;
+var startX, startY, endX, endY, startTime, endTime;
 
 document.getElementById('hoge').addEventListener('touchstart', function(evt){
   startX = evt.touches[0].screenX;
   startY = evt.touches[0].screenY;
+  startTime = new Date();
 });
 document.getElementById('hoge').addEventListener('touchend', function(evt){
   endX = evt.changedTouches[0].screenX;
   endY = evt.changedTouches[0].screenY;
+  endTime = new Date();
 
+  /*縦の移動*/
   var h = startY - endY;
+  /*横の移動*/
   var w = startX - endX;
-
+  /*距離*/
   var distance = Math.sqrt(h*h + w*w);
+  /*経過時間*/
+  var diffTime = endTime - startTime;
+  /*速度（pt/ms）*/
+  var speed = distance / diffTime;
 
-  alert(distance);
+  alert(speed);
 
 });
