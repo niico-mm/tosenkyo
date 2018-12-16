37b21cd96ee29301c76f4b4dc66fd5e3b8b143c8
板垣恵 <mm.cosmos202@gmail.com>
2018-12-16 15:53:34 +0900

    [add]判定調整

--- js/judgement.js
+++ js/judgement.js
@@ -1,12 +1,14 @@
 function judge(h, w, speed, beta, gmma) {
-  if((h >= 200) && (w <= 10) && (speed > 1)
-      && (beta <= 5) && (beta >= -5)
-      && (gamma <= 5) && (gamma >= -5)) {
-    return 2;
+  if((h >= 200) && (w <= 10) && (speed > 1) && (beta <= 5) && (beta >= -5) && (gamma <= 5) && (gamma >= -5)) {
+    return 30;
   } else if((h >= 100) && (w <= 15) && (speed > 0.5) && (beta <= 7) && (beta >= -7) && (gamma <= 7) && (gamma >= -7)) {
-    return 1;
+    return 29;
+  } else if((h >= 150) && w && speed && beta && gamma ) {
+    return ;
+  } else if( h && (w <= 50) && speed && beta && gamma ) {
+    return 29;
   } else if( h && w && speed && beta && gamma ) {
-    return 0;
+    return 1;
   } else {
     return -1;
   }
